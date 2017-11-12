75998fee3507e90186b3f2d753c743796b4a37f4
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-12 21:58:12 +0000

    Sun Nov 12 21:58:12 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -12,15 +12,16 @@ public class FizzBuzz {
     	else if ((number % 3 == 0 || String.valueOf(number).contains("3")) && (number % 5 == 0 || String.valueOf(number).contains("5")))  {
         	return "fizz buzz";
         }
+        else if (number > 10 && checkForIdentical(number)) {
+        	return "deluxe";
+        }
         else if (number % 3 == 0 || String.valueOf(number).contains("3"))  {
         	return "fizz";
         }
         else if (number % 5 == 0 || String.valueOf(number).contains("5")) {
         	return "buzz";
         }
-        else if (number > 10 && checkForIdentical(number)) {
-        	return "deluxe";
-        }
+
         else {
         	return "" + number;
         }
